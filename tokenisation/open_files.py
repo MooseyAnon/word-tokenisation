faa2cc042f9d9679f35a233fbac27a157ec067a6
Ahmed Mahdi <ahmed.mahdi@live.co.uk>
2017-11-06 21:58:53 +0000

    first word token commit

---
+++ tokenisation/open_files.py
@@ -0,0 +1,98 @@
+import re, logging, sys 
+
+
+
+
+"""These functions show how opening files can be conditionally seperated."""
+
+# conifgure logging, because we love logs! 
+
+# LOG_FORM output example = INFO 2017-10-26 21:58:20,336 - test message!
+LOG_FORM = '%(levelname)s %(asctime)s - %(message)s'
+
+logging.basicConfig(filename = 'openfiles.txt', 
+					level = logging.DEBUG, 
+					format = LOG_FORM,
+					filemode='w') # get rid of this line to persist logs
+
+logger = logging.getLogger()
+
+# uncomment and run to test logger set up
+# logger.info('test message!')
+
+
+def open_csv(afile):
+	"""open and read csv file"""
+
+	try:
+		f = open(afile, 'rb')
+		return f 
+	except IOError:
+		logger.debug("something went wrong trying to open {0}. Error: {1}".format(afile, err))
+		return err
+
+
+def open_txt(afile):
+	"""open and read a txt file"""
+
+	try:
+		f = open(afile, 'r').read()
+		return f 
+	except IOError as err:
+		logger.debug("something went wrong trying to open {0}. Error: {1}".format(afile, err))
+		return err
+
+
+def check_file_type(afile):
+	"""check file type (using the file format) and open using right function"""
+
+	pdf = re.compile('[A-Za-z0-9]+\.pdf$')
+	csv = re.compile('[A-Za-z0-9]+\.csv$')
+	txt = re.compile('[A-Za-z0-9]+\.txt$')
+
+	if bool(pdf.search(afile)) == True:
+		# opened_pdf=open_pdf(afile) # needs to be done after finding the right 3rd party libraries
+		pass
+		# opened_pdf
+
+	elif bool(csv.search(afile)) == True:
+		opened_csv = open_csv(afile) 
+		logger.debug('check_file_type opened a csv file named {0}'.format(afile))
+		return opened_csv
+	
+	elif bool(txt.search(afile)) == True:
+		opened_reg_file = open_txt(afile)
+		logger.debug('check_file_type opened a txt file named {0}'.format(afile))
+		return opened_reg_file  
+
+	else:
+		logger.debug('check_file_type cannot open this file format. File name: {0}'.format(afile))
+
+
+
+def open_file(afile):
+	"""simple open file function to act as interface to more thorough file checking functions.
+		Returns a checked and read file"""
+
+	f = check_file_type(afile)
+	logger.debug('open_file successfully opened {0}'.format(afile))
+	return f
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tokenisation/tokenisation.py
@@ -0,0 +1,103 @@
+import re, logging, csv 
+
+from nltk.corpus import stopwords
+from open_files import *
+
+
+"""
+This is a series of simple tokenisation functions. Most of these can be done using NLTK (recommended), 
+
+however, these are my own implementations to illustrate how these functions could be doing their magic 
+
+under the hood.
+
+Furthermore, oftentimes personal tokenisation is required on any given corpus in order to get as accurate results 
+
+as possible. These functions offer some easy to edit, starting strategies to go some wat to achieving those results.
+"""
+
+
+
+def split_csv_lines(afile):
+	"""split csv document into a list of lists. Each sublist corresponds with a line in the doc"""
+	f = open_file(afile)
+	reader = csv.reader(f)
+	logger.debug('split_csv_lines successfully opned and read {0}'.format(afile))
+	return [row for row in reader]
+ 
+
+def split_csv_terms(afile):
+	"""split csv document into a list of lists. Each sublist is a list of seperated terms from the line"""
+
+	f = open_file(afile)
+	reader = csv.reader(f, delimiter=' ')
+	logger.debug('split_csv_terms successfully opned and read {0}'.format(afile)) 
+	return [word for row in reader for word in row] 
+
+
+def unique_csv_terms(afile):
+	"""returns a set of unique terms in a csv document"""
+
+	f = open_file(afile)
+	reader = csv.reader(f, delimiter=' ')
+	logger.debug('unique_csv_terms successfully opned and read {0}'.format(afile))
+	uniq_word_set = set(word for row in reader for word in row) # set doesn't allow duplicates
+	return uniq_word_set
+
+
+
+def remove_punct(afile):
+	"""removes punctuation and symbols from a file. I left this as optional because 
+	sometimes it's useful to have punct/symbols for analysis. """ 
+
+	f = open_file(afile)
+	logger.debug('remove_punct successfully opned and read {0}. Attempting to remove punctuation'.format(afile))
+	return f.translate(None, '_+-.,!@#$%^&*();\/|<>"') # replaces given input them with "" wherever found in doc 
+
+
+
+# regular file with no explicit lines
+def split_file_terms(afile):
+	"""split document at space between each work and create a word list"""
+
+	f = remove_punct(afile) # change this to open_file to keep punctuation
+	logger.debug('split_file_terms successfully opned and read {0}'.format(afile))
+	return [word for word in re.split('\s+', f)]
+
+
+
+# regular file with no explicit lines unique word list
+def unique_file_terms(afile):
+	"""split document at space between each work and create a set of documents unique words"""
+
+	# this goes over every word and ommittes if word is in set already. 
+	f = open_file(afile)
+	logger.debug('unique_file_terms successfully opned and read {0}'.format(afile))
+	unique_words= set(word for word in re.split('\s+', f)) 
+	return unique_words
+
+
+
+def find_capitals(afile):
+	"""In a given document returns all words that start with a capital letter. 
+	When use along with remove_stop_words its an easy way to find documents Nouns.""" 
+
+	f = open_file(afile)
+	logger.debug('find_capitals successfully opned and read {0}'.format(afile))
+	return re.findall('[A-Z]\w+', f) 
+
+
+def remove_stop_words(afile):
+	"""returns a stopword free list"""
+
+	f= file_to_term_split(afile)
+	logger.debug('remove_stop_words successfully opned, read and split {0}'.format(afile))
+	stop_words = stopwords.words('english') # this is from the NLTk library (most common English words) but can easily be replaced with one's own list of stopwords
+	return [word for word in f if word not in stop_words] 
+
+
+
+
+
+
+
